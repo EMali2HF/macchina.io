30d112f186eb0b8de7053e9927b59839786ad337
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2017-12-14 21:24:18 +0100

    update env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 export MACCHINA_BASE=`pwd`
 export PROJECT_BASE=$MACCHINA_BASE
 export POCO_BASE=$MACCHINA_BASE/platform
-export DYLD_LIBRARY_PATH=$POCO_BASE/lib/Darwin/x86_64
-export LD_LIBRARY_PATH=$POCO_BASE/lib/Linux/x86_64:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$POCO_BASE/lib/Darwin/x86_64:$PROJECT_BASE/server/bin/Darwin/x86_64/codeCache
+export LD_LIBRARY_PATH=$POCO_BASE/lib/Linux/x86_64:$PROJECT_BASE/server/bin/Linux/x86_64/codeCache:$LD_LIBRARY_PATH
